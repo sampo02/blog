4c20015a8054cfac5c50b7546613e153c4c0f341
sampo02 <m.nishitsuji@gmail.com>
2021-06-06 10:08:52 +0900

    :sparkles: Add markdown settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: "sampo02-web-log",
+    title: "sampo02 web log",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
