bdd6370358e202d67d91cc0a076e9876080a6a11
sampo02 <m.nishitsuji@gmail.com>
2021-06-20 00:00:50 +0900

    🔧 Configure favicon (#32)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ module.exports = {
     title: "sampo02's blog"
   },
   plugins: [
+    'gatsby-plugin-graphql-codegen',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-transformer-remark',
