f6eb0034b37cc8dc59289d15e378ed02d4e8ed03
sampo02 <m.nishitsuji@gmail.com>
2021-07-14 16:49:04 +0900

    :wrench: Configure remove tailing slashes

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-remove-trailing-slashes',
     'gatsby-plugin-robots-txt',
     'gatsby-plugin-sass',
     'gatsby-plugin-sitemap',
